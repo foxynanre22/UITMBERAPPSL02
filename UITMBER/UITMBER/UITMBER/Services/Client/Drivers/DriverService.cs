92b308df1459af260b6dce444fac67f45982645e
Irynaa123 <irynapukish68@gmail.com>
2021-01-30 16:42:39 +0100

    Add GetNerbyDriveres, GetProfile( Driver)

---
+++ UITMBER/UITMBER/UITMBER/Services/Client/Drivers/DriverService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UITMBER.Models;
+using UITMBER.Models.Dto;
+using UITMBER.Services.Request;
+
+namespace UITMBER.Services.Client.Drivers
+{
+    class DriverService : IDriversService
+    {
+        private RequestService _requestService;
+
+        public DriverService()
+        {
+            _requestService = new RequestService();
+        }
+
+        public async Task<List<DriverDto>> GetNerbyDriveres(LatLong latlong)
+        {
+            var url = $"/Driver/GetNerbyDriveres?latitude={latlong.Lat}&longitude={latlong.Long}";
+            var data = await _requestService.GetAsync<List<DriverDto>>(url);
+
+            return data;
+        }
+
+        public async Task<DriverDto> GetProfile(int driverId)
+        {
+            var url = $"/Driver/GetProfile?id={driverId}";
+            var data = await _requestService.GetAsync<DriverDto>(url);
+
+            return data;
+        }
+    }
+}
