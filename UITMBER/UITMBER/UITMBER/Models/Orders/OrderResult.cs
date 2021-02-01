eb0321ee51be730df6df23b5bc040c6f6d549b53
Michasssss <michaeldzindzio@gmail.com>
2021-02-02 00:50:40 +0100

    Orders
    GET/Orders/GetClientOrderDetails
    GET/Orders/GetLuggageTypes
    GET/Orders/GetCost

---
+++ UITMBER/UITMBER/UITMBER/Models/Orders/OrderResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UITMBER.Enums;
+
+namespace UITMBER.Models.Orders
+{
+    public class OrderResult
+    {
+        public long UserId { get; set; }
+        //Trzeba dodać w api
+        public long IdOrder { get; set; }
+        public long DriverId { get; set; }
+        public double? ClientRating { get; set; }
+        public double? DriverRating { get; set; }
+        public CarType Type { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Photo { get; set; }
+        public string PhoneNumber { get; set; }
+        public double StartLat { get; set; }
+        public double StartLong { get; set; }
+        public double EndLat { get; set; }
+        public double EndLong { get; set; }
+        public double Cost { get; set; }
+    }
+}
---
+++ UITMBER/UITMBER/UITMBER/Services/Orders/OrderService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UITMBER.Models.Orders;
+using UITMBER.Services.Request;
+using Xamarin.Forms;
+
+namespace UITMBER.Services.Orders
+{
+    public class OrderService : IOrderService
+    {
+        public IRequestService _requestService => DependencyService.Get<IRequestService>();
+
+        public async Task<List<OrderResult>> GetClientOrderDetails()
+        {
+            var uri = $"{Settings.SERVER_ENDPOINT}/Orders/GetClientOrderDetails";
+
+
+            return await _requestService.GetAsync<List<OrderResult>>(uri);
+    
+        }
+
+        public async Task<List<string>> GetLuggageTypes()
+        {
+            var uri = $"{Settings.SERVER_ENDPOINT}/Orders/GetLuggageTypes";
+
+
+            return await _requestService.GetAsync<List<string>>(uri);
+
+        }
+
+        public async Task<double> GetCost(DateTime date, double distance)
+        {
+            var uri = $"{Settings.SERVER_ENDPOINT}/Orders/GetCost?date={date}&distance={distance}";
+
+
+            return await _requestService.GetAsync<double>(uri);
+
+        }
+
+
+    }
+}
