c00a49f7911fe5b3455a104f96e602c8877e658d
mmarochkanych <marochkanychmariia@gmail.com>
2021-02-02 17:42:29 +0100

    Orders: Add GetMyOrders, GetCarTypes
    GET/Orders​/GetMyOrders
    GET/Orders​/GetCarTypes

---
+++ UITMBER/UITMBER/UITMBER/Services/Orders/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UITMBER.Models.Orders;
+
+namespace UITMBER.Services.Orders
+{
+    public interface IOrderService
+    {
+         Task<List<OrderResult>> GetMyOrders();
+         Task<List<OrderResult>> GetCarTypes();
+    }
+}
---
+++ UITMBER/UITMBER/UITMBER/Services/Orders/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UITMBER.Models.Orders;
+using UITMBER.Services.Request;
+using Xamarin.Forms;
+
+namespace UITMBER.Services.Orders
+{
+    public class OrderService : IOrderService
+    {
+        public IRequestService _requestService => DependencyService.Get<IRequestService>();
+
+        public async Task<List<OrderResult>> GetMyOrders()
+        {
+            var uri = $"{Settings.SERVER_ENDPOINT}/Orders/GetMyOrders";
+
+
+            return await _requestService.GetAsync<List<OrderResult>>(uri);
+    
+        }
+
+        public async Task<List<OrderResult>> GetCarTypes()
+        {
+            var uri = $"{Settings.SERVER_ENDPOINT}/Orders/GetCarTypes";
+
+
+            return await _requestService.GetAsync<List<OrderResult>>(uri);
+
+        }
+    }
+}
