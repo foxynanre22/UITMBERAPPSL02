946ab974a92fee5afc24ff66934a39bf21f3c710
Irynaa123 <irynapukish68@gmail.com>
2021-02-03 09:17:40 +0100

    DI Code refract, urlBaseAddress fix

--- UITMBER/UITMBER/UITMBER/App.xaml.cs
+++ UITMBER/UITMBER/UITMBER/App.xaml.cs
@@ -5,6 +5,7 @@ using UITMBER.Services;
 using UITMBER.Views;
 using UITMBER.Services.Request;
 using UITMBER.Services.Authentication;
+using UITMBER.Services.Client.Drivers;
 
 namespace UITMBER
 {
@@ -38,7 +39,8 @@ namespace UITMBER
 
             DependencyService.Register<IRequestService, RequestService>();
             DependencyService.Register<IAuthenticationService, AuthenticationService>();
-            
+
+            DependencyService.Register<IDriversService, DriverService>();
         }
 
     }
--- UITMBER/UITMBER/UITMBER/Services/Client/Drivers/DriverService.cs
+++ UITMBER/UITMBER/UITMBER/Services/Client/Drivers/DriverService.cs
@@ -5,21 +5,17 @@ using System.Threading.Tasks;
 using UITMBER.Models;
 using UITMBER.Models.Dto;
 using UITMBER.Services.Request;
+using Xamarin.Forms;
 
 namespace UITMBER.Services.Client.Drivers
 {
     class DriverService : IDriversService
     {
-        private RequestService _requestService;
-
-        public DriverService()
-        {
-            _requestService = new RequestService();
-        }
+        private IRequestService _requestService => DependencyService.Get<IRequestService>();
 
         public async Task<List<DriverDto>> GetNerbyDriveres(LatLong latlong)
         {
-            var url = $"/Driver/GetNerbyDriveres?latitude={latlong.Lat}&longitude={latlong.Long}";
+            var url = $"{Settings.SERVER_ENDPOINT}/Driver/GetNerbyDriveres?latitude={latlong.Lat}&longitude={latlong.Long}";
             var data = await _requestService.GetAsync<List<DriverDto>>(url);
 
             return data;
@@ -27,7 +23,7 @@ namespace UITMBER.Services.Client.Drivers
 
         public async Task<DriverDto> GetProfile(int driverId)
         {
-            var url = $"/Driver/GetProfile?id={driverId}";
+            var url = $"{Settings.SERVER_ENDPOINT}/Driver/GetProfile?id={driverId}";
             var data = await _requestService.GetAsync<DriverDto>(url);
 
             return data;
