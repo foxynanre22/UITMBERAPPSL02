e1b79aa4d3c611deab641d5178557f480f3a5f95
Kamilgolda <kamil.goldaat@gmail.com>
2021-02-11 11:51:29 +0100

    LoginPage cd + RecoverAccountPage

--- UITMBER/UITMBER/UITMBER/ViewModels/LoginViewModel.cs
+++ UITMBER/UITMBER/UITMBER/ViewModels/LoginViewModel.cs
@@ -27,22 +27,37 @@ namespace UITMBER.ViewModels
             set => SetProperty(ref password, value);
         }
 
-        AuthenticationService authenticationService = new AuthenticationService();
+        AuthenticationService authenticationService => new AuthenticationService();
         
         public Command LoginCommand { get; }
+        public Command RegisterCommand { get; }
+        public Command RecoverCommand { get; }
 
         public LoginViewModel()
         {
 
             LoginCommand = new Command(OnLoginClicked);
+            RegisterCommand = new Command(OnRegisterClicked);
+            RecoverCommand = new Command(OnRecoverClicked);
+        }
+
+        private async void OnRecoverClicked(object obj)
+        {
+            await Shell.Current.GoToAsync("//RecoverAccountPage");
+            
+        }
+
+        private async void OnRegisterClicked(object obj)
+        {
+            await Shell.Current.GoToAsync("//RegisterPage");
         }
 
         private async void OnLoginClicked(object obj)
         {
             await authenticationService.Authenticate(new Models.Authentication.AuthenticationRequest
             {
-                Login = Email,
-                Password = Password
+                Login = email,
+                Password = password
             });
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
             await Shell.Current.GoToAsync($"//{nameof(MainPage)}");
